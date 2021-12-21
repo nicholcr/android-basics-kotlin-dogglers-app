274eb7166e264f814c7be90eb436564775f39bd6
Nick Crumb <nicholcr@umich.edu>
2021-12-20 20:15:21 -0500

    finished tutorial

--- app/src/main/java/com/example/dogglers/GridListActivity.kt
+++ app/src/main/java/com/example/dogglers/GridListActivity.kt
@@ -19,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dogglers.adapter.DogCardAdapter
 import com.example.dogglers.const.Layout
+import com.example.dogglers.data.DataSource
 import com.example.dogglers.databinding.ActivityGridListBinding
 
 class GridListActivity : AppCompatActivity() {
